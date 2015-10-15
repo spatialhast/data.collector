50df14265cba0b4654469fca8192af59afe0356e
Alexander <a.hast@mail.ru>
2015-10-15 10:56:44 +0300

    update page view

--- php/callProxy.php
+++ php/callProxy.php
@@ -1,23 +1,5 @@
 <?php
-//include 'cartoDBProxy.php';
-
-session_cache_limiter('nocache');
-$cache_limiter = session_cache_limiter();
-function goProxy($dataURL) 
-{
-	$baseURL = 'http://hast.cartodb.com/api/v2/sql?';
-	//  					^ CHANGE THE 'CARTODB-USER-NAME' to your cartoDB url!
-	$api = '&api_key=b047b69708b0b5c661c4d0dc228dc78688055388';
-	//				 ^ENTER YOUR API KEY HERE!
-	$url = $baseURL.'q='.urlencode($dataURL).$api;
-	
-	echo "<script type='text/javascript'>alert('$url');</script>";
-	
-	$result = file_get_contents ($url);
-		
-	return $result;
-}
-
+include 'cartoDBProxy.php';
 //			^CHANGE THIS TO THE PATH TO YOUR cartodbProxy.php
 $queryURL = $_POST['qurl'];
 $return = goProxy($queryURL);
--- php/cartoDBProxy.php
+++ php/cartoDBProxy.php
@@ -3,9 +3,9 @@ session_cache_limiter('nocache');
 $cache_limiter = session_cache_limiter();
 function goProxy($dataURL) 
 {
-	$baseURL = 'http://hast.cartodb.com/api/v2/sql?';
+	$baseURL = 'http://{USER}.cartodb.com/api/v2/sql?';
 	//  					^ CHANGE THE 'CARTODB-USER-NAME' to your cartoDB url!
-	$api = '&api_key=b047b69708b0b5c661c4d0dc228dc78688055388';
+	$api = '&api_key={API_KEY}';
 	//				 ^ENTER YOUR API KEY HERE!
 	$url = $baseURL.'q='.urlencode($dataURL).$api;
 	
